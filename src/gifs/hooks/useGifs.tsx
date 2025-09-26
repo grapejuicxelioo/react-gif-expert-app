5fbdd9357993c413106cc30d9d7833fce0d55ca6
eliezzer <140941254+eliezzerzpdda@users.noreply.github.com>
2025-09-25 18:34:20 -0700

    Manejo en caché + useRef
    Mantener el valor entre re-renders

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { getGifsByQuery } from '../actions/get-gifs-by-query.action';
 import type { Gif } from '../interfaces/gif.interface';
+
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
     const [gifs, setGifs] = useState<Gif[]>([]);
     const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
-    const handleTermClicked = (term: string) => {
-        console.log({ term })
+    const gifsCache = useRef<Record<string, Gif[]>>({});
+
+    const handleTermClicked = async (term: string) => {
+        if (gifsCache.current[term]) {
+            setGifs(gifsCache.current[term]);
+            return;
+        }
+
+        const gifs = await getGifsByQuery(term);
+        setGifs(gifs);
     };
     const handleSearch = async (query: string = '') => {
         query = query.trim().toLowerCase();
@@ -24,6 +34,8 @@ export const useGifs = () => {
 
         const gifs = await getGifsByQuery(query);
         setGifs(gifs);
+
+        gifsCache.current[query] = gifs;
     };
 
     return {
